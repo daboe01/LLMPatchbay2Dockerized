24f5ab19b8f5571d62882d7d3a26aeaea4aa9ec7
daboe01 <daboe01@googlemail.com>
2025-07-23 14:19:13 +0200

    debug

--- Application/backend.pl
+++ Application/backend.pl
@@ -552,7 +552,7 @@ $r->put('/LLM/:table/:pk/:key' => [key => qr/\d+/] => sub
         $self->pg->db->update($self->param('table'), $u, {$self->param('pk') => $self->param('key')});
     };
     if ($@) {
-        return $self->render(json => {err => $self->req->body});
+        return $self->render(json => {err => decode 'UTF-8', $self->req->body});
     }
 
     $self->render(json => {err => $DBI::errstr});
